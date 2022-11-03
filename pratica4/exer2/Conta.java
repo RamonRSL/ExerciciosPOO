90815d151179859ecc4fcecfa2d86088e17e3a5e
RamonRSL <111541435+RamonRSL@users.noreply.github.com>
2022-11-03 16:59:48 -0300

    Add files via upload

---
+++ pratica4/exer2/Conta.java
@@ -0,0 +1,9 @@
+package br.com.newtonpaiva.pratica4.exer2;
+
+public interface Conta {
+	
+	void depositar(double valor);
+	void sacar(double valor);
+	double getSaldo();
+	
+}
---
+++ pratica4/exer2/GeradorExtratos.java
@@ -0,0 +1,9 @@
+package br.com.newtonpaiva.pratica4.exer2;
+
+public class GeradorExtratos {
+	
+public void geradorConta (Conta conta) {
+	System.out.println("Saldo Atual: " + conta.getSaldo());
+
+	}
+}
